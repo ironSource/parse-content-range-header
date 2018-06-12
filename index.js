b0c92e3269cc4a38eb6e3b3a8b62bebc570a50c4
kessler <yanivk@gmail.com>
2018-06-12 20:33:03 +0300

    initial

---
+++ index.js
@@ -0,0 +1,88 @@
+module.exports = parseContentRangeHeader
+
+/**
+ *    @param  {String} headerValue - the value of a `Content-Range` header
+ *    	The supported forms of this header are specified in this [RFC](https://tools.ietf.org/html/rfc7233#section-4.2)
+ *    	and on [MDN](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Range#Syntax)
+ *
+ *    @return {ParseResult}
+ *
+ */
+function parseContentRangeHeader(headerValue) {
+	let unitSeparator = headerValue.indexOf(' ')
+
+	if (unitSeparator === -1) {
+		throw new Error('missing unit separator')
+	}
+
+	let unit = headerValue.substr(0, unitSeparator)
+
+	if (unit.length === 0) {
+		throw new Error('missing unit value')
+	}
+
+	let sizeSeparator = headerValue.indexOf('/', unitSeparator + 1)
+
+	if (sizeSeparator === -1) {
+		throw new Error('missing size separator')
+	}
+
+	let rangeSeparator = headerValue.indexOf('-', unitSeparator + 1)
+
+	let range
+	let isRangeSatisfiable
+
+	if (rangeSeparator > -1) {
+		let rangeStart = parseInt(headerValue.substring(unitSeparator + 1, rangeSeparator))
+		if (isNaN(rangeStart)) {
+			throw new Error('invalid range start value')
+		}
+
+		let rangeEnd = parseInt(headerValue.substring(rangeSeparator + 1, sizeSeparator))
+		if (isNaN(rangeEnd)) {
+			throw new Error('invalid range end value')
+		}
+
+		if (rangeStart > rangeEnd) {
+			throw new Error('range start is greater than range end')
+		}
+
+		range = { start: rangeStart, end: rangeEnd }
+		isRangeSatisfiable = true
+	} else {
+		range = headerValue.substring(unitSeparator + 1, sizeSeparator)
+		if (range !== '*') {
+			throw new Error('invalid range')
+		}
+
+		isRangeSatisfiable = false
+	}
+
+	let size = headerValue.substring(sizeSeparator + 1)
+	let isSizeKnown
+
+	if (size === '*') {
+		isSizeKnown = false
+
+	} else {
+		size = parseInt(size)
+		if (isNaN(size)) {
+			throw new Error('invalid size value')
+		}
+
+		isSizeKnown = true
+	}
+
+	/**
+	 *    @name ParseResult
+	 *    @type {Object}
+	 *    @property {Object|String} range - range start and range end, if range is not satisfiable (`isRangeSatisfiable === false`), then this will be a `*`
+	 *    @property {Number} range.start
+	 *    @property {Number} range.end
+	 *    @property {Boolean} isRangeSatisfiable - a flag indicating if the server can satisfy the range request or not
+	 *    @property {String} unit - the unit of the range, usually this will be `bytes`
+	 *    @property {Number|String} size - the full size of the resource. If the size is not known (`isSizeKnown === false`) then this will be a `*`
+	 *    @property {Boolean} isSizeKnown - a flag indicating if the server knows the size of the resource or not
+	 */
+	return { range, isRangeSatisfiable, unit, size, isSizeKnown }
+}
---
+++ test.js
@@ -0,0 +1,96 @@
+const { expect } = require('chai')
+const parseContentRangeHeader = require('./index')
+
+describe('parseContentRangeHeader', () => {
+	it('parses a Content-Range header <unit> <range start>-<range end>/<size>', () => {
+		let header = 'bytes 0-500/1000'
+		let actual = parseContentRangeHeader(header)
+		expect(actual).to.eql({
+			isRangeSatisfiable: true,
+			range: { start: 0, end: 500 },
+			unit: 'bytes',
+			isSizeKnown: true,
+			size: 1000
+		})
+	})
+
+	it('parses a Content-Range header <unit> */<size>', () => {
+		let header = 'bytes */1000'
+		let actual = parseContentRangeHeader(header)
+		expect(actual).to.eql({
+			isRangeSatisfiable: false,
+			range: '*',
+			unit: 'bytes',
+			isSizeKnown: true,
+			size: 1000
+		})
+	})
+
+	it('parses a Content-Range header <unit> <range start>-<range end>/*', () => {
+		let header = 'bytes 0-50/*'
+		let actual = parseContentRangeHeader(header)
+		expect(actual).to.eql({
+			isRangeSatisfiable: true,
+			range: { start: 0, end: 50},
+			unit: 'bytes',
+			isSizeKnown: false,
+			size: '*'
+		})
+	})
+
+	it('throws an error if unit separator is missing', () => {
+		let header = 'bytes0-500/1000'
+		expect(() => {
+			parseContentRangeHeader(header)
+		}).to.throw(Error, 'missing unit separator')
+	})
+
+	it('throws an error if unit are missing', () => {
+		let header = ' '
+		expect(() => {
+			parseContentRangeHeader(header)
+		}).to.throw(Error, 'missing unit value')
+	})
+
+	it('throws an error if range is invalid', () => {
+		let header = 'bytes 0500/1000'
+		expect(() => {
+			parseContentRangeHeader(header)
+		}).to.throw(Error, 'invalid range')
+	})
+
+	it('throws an error if range start is not an integer', () => {
+		let header = 'bytes kjhas-500/1000'
+		expect(() => {
+			parseContentRangeHeader(header)
+		}).to.throw(Error, 'invalid range start value')
+	})
+
+	it('throws an error if size separator is missing', () => {
+		let header = 'bytes 0-100'
+		expect(() => {
+			parseContentRangeHeader(header)
+		}).to.throw(Error, 'missing size separator')
+	})
+
+	it('throws an error if range end is not an integer', () => {
+		let header = 'bytes 0-i500/1000'
+		expect(() => {
+			parseContentRangeHeader(header)
+		}).to.throw(Error, 'invalid range end value')
+	})
+
+	it('throws an error if range start is greater than range end', () => {
+		let header = 'bytes 10-5/1000'
+		expect(() => {
+			parseContentRangeHeader(header)
+		}).to.throw(Error, 'range start is greater than range end')
+	})
+
+	it('throws an error if size is not an integer', () => {
+		let header = 'bytes 0-500/lkjs'
+		expect(() => {
+			parseContentRangeHeader(header)
+		}).to.throw(Error, 'invalid size value')
+	})
+})
